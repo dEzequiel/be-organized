aa1034bd8cebe452acb9f6a140b6c01eaa011122
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-04-19 22:22:19 +0200

    test: add tests for bubble sort method

--- test/bubble_sort_test.test.ts
+++ test/bubble_sort_test.test.ts
@@ -1,9 +1,13 @@
 import { Sorter } from "../src/bubble_sort";
 
-const sorter = new Sorter([3, 2]);
+const sorter = new Sorter();
 
 describe("test bubble function", () => {
     it("should return [2, 3] for bubble_sort([3, 2])", () => {
-      expect(sorter.bubble_sort()).toStrictEqual([2, 3]);
+      expect(sorter.bubble_sort([3, 2])).toStrictEqual([2, 3]);
+    })
+
+    it("should return [2, 3, 4] for bubble_sort([4, 3, 2])", () => {
+      expect(sorter.bubble_sort([3, 2, 1])).toStrictEqual([1, 2, 3]);
     })
 });
