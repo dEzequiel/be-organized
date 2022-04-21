2f75498ce3c8f98c3416c18d65d4d3de04f20f4d
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-04-22 00:08:18 +0200

    feat: add Sorter class to sort any kind of data structure if its Sortable type

---
+++ src/Sorter.ts
@@ -0,0 +1,21 @@
+import { Sortable } from "./interfaces/Sortable";
+
+export class Sorter {
+
+    collection: Sortable;
+    constructor(collection: Sortable) {
+        this.collection = collection;
+    }
+
+    sort(): void {
+        const { length } = this.collection;
+
+        for(let i = 0; i < length; i++) {
+            for (let j = 0; j < length; j++) {
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
+                }
+            }
+        }
+    }
+}
