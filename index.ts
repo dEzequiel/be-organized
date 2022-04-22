352175ca624ce1e286eca5b92ad27b156c7456f4
Eze Dlr <erosarosario@cifpfbmoll.com>
2022-04-22 12:51:18 +0200

    feat: call sort() directly from objects instances

--- index.ts
+++ index.ts
@@ -4,10 +4,14 @@ import { CharactersCollection } from "./src/CharactersCollection";
 
 const numbersCollection = new NumbersCollection([5, 4, 3, 2, 1]);
 const charactersCollection = new CharactersCollection('iduahfjnladfilhAHHASFDJLSHDL');
-const sorter = new Sorter(numbersCollection);
-const sorterString = new Sorter(charactersCollection);
 
-sorter.sort();
-sorterString.sort()
+// const sorter = new Sorter(numbersCollection); ** ahorrado **
+// const sorterString = new Sorter(charactersCollection); ** ahorrado **
+
+numbersCollection.sort();
+charactersCollection.sort();
+
+// sorter.sort(); ** ahorrado **
+// sorterString.sort() ** ahorrado **
 console.log(numbersCollection.collection);
 console.log(charactersCollection.collection);
